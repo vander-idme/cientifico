e42e7d337399a5ed1cce3693134882c21d6d80b6
vander-idme <idmevander@gmail.com>
2020-04-11 20:31:02 -0500

    Implementando y probando conexiones

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import Header from '../templates/header'
 import Home from '../pages/Home'
 import Character from '../pages/Character'
 import Error404 from '../pages/Error404'
+import getHash from '../utils/getHash'
+import resolveRoutes from '../utils/resolveRoutes'
 
 const routes = {
    '/': Home,
@@ -14,6 +16,14 @@ const router = async () => {
    const content = null || document.getElementById('content')
 
    header.innerHTML = await Header()
+
+   let hash = getHash();
+   console.log(hash)
+   let route = await resolveRoutes(hash)
+   console.log(route)
+   let render = routes[route]? routes[route] : Error404
+   
+   content.innerHTML = await render()
 }
 
 export default router
