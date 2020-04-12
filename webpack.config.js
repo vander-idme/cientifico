dcac59795ec01dccb7d483131e7b7fb47307a02f
vander-idme <idmevander@gmail.com>
2020-04-11 21:36:14 -0500

    Agregando los estilos

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin =  require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
    entry: path.resolve(__dirname, 'src', 'index.js'),
@@ -29,6 +30,10 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
          }
-      )
+      ),
+      new CopyWebpackPlugin([{
+         from: 'src/styles/styles.css',
+         to: ''
+      }])
    ]
 }
