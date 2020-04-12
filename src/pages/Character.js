141c16f6ec70a339a14cc4c5d5061a301bc0b7ab
vander-idme <idmevander@gmail.com>
2020-04-11 21:25:10 -0500

    Conectar con la API y renderizar los resultados del fetch

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,18 +1,23 @@
-const Character = () => {
+import getData from '../utils/getData'
+import getHash from '../utils/getHash'
+
+const Character = async () => {
+   const id = getHash()
+   const character = await getData(id)
    const view = `
       <div class="Characters-inner">
          <article class="Characters-card">
-            <img src="image" alt="name">
-            <h2>Name</h2>
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
          <article/>
 
          <article class="Character-card">
-            <h3>Episodes:</h3>
-            <h3>Status:</h3>
-            <h3>Species:</h3>
-            <h3>Gender:</h3>
-            <h3>Origin:</h3>
-            <h3>Last Location:</h3>
+            <h3>Episodes:<span> ${character.episode.length}</span></h3>
+            <h3>Status: ${character.status}</h3>
+            <h3>Species: ${character.species}</h3>
+            <h3>Gender: ${character.gender}</h3>
+            <h3>Origin: ${character.origin.name}</h3>
+            <h3>Last Location: ${character.location.name}</h3>
          <article/>
       </div>
    `
